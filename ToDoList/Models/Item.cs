fa8097f8942b93a3420a3025e695f95420c0ef6c
Chris Loveless <chriskloveless@gmail.com>
2022-12-12 19:35:41 -0800

    create MVC file structure

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
     public class Item
     {
+        public string Description { get; set; }
 
+        // Let's declare a static variable that maintains a list of all Item objects like this:
+        private static List<Item> _instances = new List<Item> { };
+
+        public Item(string description)
+        {
+            Description = description;
+            _instances.Add(this); // New code.
+        }
+
+        public static List<Item> GetAll()
+        {
+            return _instances;
+        }
+
+				public static void ClearAll()
+				{
+					_instances.Clear();
+				}
     }
 }
