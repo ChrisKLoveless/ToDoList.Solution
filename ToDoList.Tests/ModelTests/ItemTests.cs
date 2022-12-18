72b45b54bc85d0809f1a09fd1d9f742c1b052291
Chris Loveless <chriskloveless@gmail.com>
2022-12-18 15:41:21 -0800

    add lesson code to project WIP: tests fail: error access denied for localhost

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Configuration;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System;
@@ -9,9 +10,21 @@ namespace ToDoList.Tests
   public class ItemTests : IDisposable
   {
 
+    // we've added a new property
+    public IConfiguration Configuration { get; set; }
+
     public void Dispose()
     {
       Item.ClearAll();
+    }
+
+    // we've added a constructor
+    public ItemTests()
+    {
+      IConfigurationBuilder builder = new ConfigurationBuilder()
+          .AddJsonFile("appsettings.json");
+      Configuration = builder.Build();
+      DBConfiguration.ConnectionString = Configuration["ConnectionStrings:TestConnection"];
     }
 
     [TestMethod]
@@ -48,7 +61,7 @@ namespace ToDoList.Tests
       Assert.AreEqual(updatedDescription, result);
     }
     [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
+    public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
     {
       // Arrange
       List<Item> newList = new List<Item> { };
@@ -67,7 +80,9 @@ namespace ToDoList.Tests
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
       Item newItem1 = new Item(description01);
+      newItem1.Save(); // New code
       Item newItem2 = new Item(description02);
+      newItem2.Save(); // New code
       List<Item> newList = new List<Item> { newItem1, newItem2 };
 
       //Act
@@ -76,6 +91,7 @@ namespace ToDoList.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
+
 
     [TestMethod]
     public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
@@ -92,22 +108,45 @@ namespace ToDoList.Tests
     }
 
     [TestMethod]
-    public void Find_ReturnsCorrectItem_Item()
+    public void Find_ReturnsCorrectItemFromDatabase_Item()
     {
       //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
+      Item newItem = new Item("Mow the lawn");
+      newItem.Save();
+      Item newItem2 = new Item("Wash dishes");
+      newItem2.Save();
 
       //Act
-      Item result = Item.Find(2);
-
+      Item foundItem = Item.Find(newItem.Id);
       //Assert
-      Assert.AreEqual(newItem2, result);
+      Assert.AreEqual(newItem, foundItem);
     }
 
+    [TestMethod]
+    public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+    {
+      // Arrange, Act
+      Item firstItem = new Item("Mow the lawn");
+      Item secondItem = new Item("Mow the lawn");
 
+      // Assert
+      Assert.AreEqual(firstItem, secondItem);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_ItemList()
+    {
+      //Arrange
+      Item testItem = new Item("Mow the lawn");
+
+      //Act
+      testItem.Save();
+      List<Item> result = Item.GetAll();
+      List<Item> testList = new List<Item> { testItem };
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
   }
 }
 
